7dd5035b28692aedba5abf0b21ea0d2a20b45ba1
het-tale <ettalebyhasnaa99@gmail.com>
2022-09-29 19:04:25 +0100

    The test worked --still segv and leaks--

---
+++ execution/close.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   close.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: het-tale <het-tale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 18:05:25 by het-tale          #+#    #+#             */
+/*   Updated: 2022/09/29 19:02:07 by het-tale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	close_pipes(t_exec *exec, int n)
+{
+	int	j;
+
+	j = 0;
+	while (j < n)
+	{
+		if (exec->fd_pipe[j] != exec->input
+			&& exec->fd_pipe[j] != exec->output)
+			close(exec->fd_pipe[j]);
+		j++;
+	}
+}
+
+void	close_and_free(t_exec exec, int n)
+{
+	int	i;
+
+	i = 0;
+	close(exec.infile);
+	close(exec.out_file);
+	// unlink("temp_file");
+	while (i < (n - 1) * 2)
+	{
+		close(exec.fd_pipe[i]);
+		i++;
+	}
+	waitpid(-1, &(exec.status), 0);
+	free(exec.child_pid);
+	free(exec.fd_pipe);
+}
